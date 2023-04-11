18f7c273d2874fe8df2f4150b9795bb22433a6f7
CAIO ESTEVAO DE CAMARGO OLIVEIRA <caio.estevao@alunoescolassociais.org.br>
2023-04-11 11:38:24 -0300

    js

---
+++ table.js
@@ -0,0 +1,26 @@
+function colorirData(){
+let day = document.getElementById("day").value;
+let color = document.getElementById("color").value;
+let clndr = document.getElementById("calendar");
+ let date = clndr.getElementsByTagName("td")[parseInt(day)+5];
+         date.style.backgroundColor = color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
